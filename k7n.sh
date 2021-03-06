272bbff0e6985ab5d765d798e2dca2b080011b44
JakWai01 <jakob.waibel@web.de>
2021-03-06 21:13:40 +0100

    feat: Add installation commands

--- k7n.sh
+++ k7n.sh
@@ -9,7 +9,8 @@
 #
 # Run Information: This script needs to be run once to setup your whole kubernetes cluster with k3s and n2n. You can run this script on any machine to install your cluster remotely.
 #
-
+token = "token"
+supernode_ip;
 i=1
 for ip in "$@"
 do  
@@ -17,13 +18,29 @@ do
     then 
         echo "Supernode: $ip"
         echo "IP-$i: $ip";
+        supernode_ip = $ip
         ssh root@${ip} << EOF
-            mkdir hello
-            mkdir hello2
+            curl -L -o /tmp/apt-ntop-stable.deb https://packages.ntop.org/apt-stable/buster/all/apt-ntop-stable.deb
+            apt install -y /tmp/apt-ntop-stable.deb
+            apt update
+            apt install -y n2n 
+            supernode -l 7777 &
+            edge -A3 -c name -k password -a 192.168.100.1 -f -l ${ip}:7777 &
+
+            curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.100.1 --flannel-iface=edge0" sh -
+            cat /var/lib/rancher/k3s/server/node-token > token.txt
 EOF
     else
         echo "IP-$i: $ip";
-        ssh root@${ip} "mkdir hello"
+        ssh root@${ip} << EOF
+            curl -L -o /tmp/apt-ntop-stable.deb https://packages.ntop.org/apt-stable/buster/all/apt-ntop-stable.deb
+            apt install -y /tmp/apt-ntop-stable.deb
+            apt update
+            apt install -y n2n
+            edge -A3 -c code-and-chill -k cacpass -a 192.168.100.${i} -f -l ${supernode_ip}:7777 &
+
+            curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --token=${token} --node-ip=192.168.100.${i} --server=https://192.168.100.1:6443 --flannel-iface=edge0" sh -
+EOF
     fi
     i=$((i + 1));
 done
