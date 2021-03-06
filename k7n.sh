32c569a74c077f92fdc2bb4df1dc6dc1f93bb01c
JakWai01 <jakob.waibel@web.de>
2021-03-06 17:24:27 +0100

    feat: Add comments with general information to script

---
+++ k7n.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Script Name: k7n.sh
+#
+# Author: Jakob Waibel
+# Date : 2020-03-06
+#
+# Description: Remote setup k3s and n2n (Kubernetes behind a VPN) automatically and from remote 
+#
+# Run Information: This script needs to be run once to setup your whole kubernetes cluster with k3s and n2n. You can run this script on any machine to install your cluster remotely.
+#
