c7dd9f162bce7042540cebf73885f1da24e48f39
JakWai01 <jakob.waibel@web.de>
2021-03-07 10:51:36 +0100

    feat: Add supernode service

--- k7n.sh
+++ k7n.sh
@@ -19,28 +19,10 @@ do
         echo "Supernode: $ip"
         echo "IP-$i: $ip";
         supernode_ip = $ip
-        ssh root@${ip} << EOF
-            curl -L -o /tmp/apt-ntop-stable.deb https://packages.ntop.org/apt-stable/buster/all/apt-ntop-stable.deb
-            apt install -y /tmp/apt-ntop-stable.deb
-            apt update
-            apt install -y n2n 
-            supernode -l 7777 &
-            edge -A3 -c name -k password -a 192.168.100.1 -f -l ${ip}:7777 &
-
-            curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.100.1 --flannel-iface=edge0" sh -
-            cat /var/lib/rancher/k3s/server/node-token > token.txt
-EOF
+        ssh root@${ip}
     else
-        echo "IP-$i: $ip";
-        ssh root@${ip} << EOF
-            curl -L -o /tmp/apt-ntop-stable.deb https://packages.ntop.org/apt-stable/buster/all/apt-ntop-stable.deb
-            apt install -y /tmp/apt-ntop-stable.deb
-            apt update
-            apt install -y n2n
-            edge -A3 -c code-and-chill -k cacpass -a 192.168.100.${i} -f -l ${supernode_ip}:7777 &
-
-            curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --token=${token} --node-ip=192.168.100.${i} --server=https://192.168.100.1:6443 --flannel-iface=edge0" sh -
-EOF
+        echo "IP-$1: $ip";
     fi
+    echo "Back home"
     i=$((i + 1));
 done
